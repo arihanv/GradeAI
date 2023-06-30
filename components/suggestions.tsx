97afac08a525919a9aa103fdcc711d944e32b7b4
arihanv <arihanvaranasi@gmail.com>
2023-06-30 18:05:59 -0500

    Adjustments

--- components/suggestions.tsx
+++ components/suggestions.tsx
@@ -1,12 +1,27 @@
-import React from "react";
+"use client"
+import React, { useState } from "react";
+import axios from "axios";
+
 
 type Props = {};
 
 export default function Suggestions({}: Props) {
+  // const [ data, setData ] = useState<any[]>([]); 
+  // React.useEffect(() => {
+  //   axios.get("https://grade-ai-ed153ea53b42.herokuapp.com/")
+  //   .then((res) => {
+  //     setData(res.data);
+  //     console.log(res.data);
+  //   })
+  //   .catch((error) => {
+  //     console.error(error);
+  //   });
+  
+  // }, []);
   return (
     <div className="w-full flex flex-col gap-5">
-      <div className=" bg-darkpaco w-full md:flex justify-center items-center px-36 py-16  hidden rounded-sm">Suggestions</div>
-      <div className=" bg-darkpaco w-full md:flex justify-center items-center px-36 py-16  hidden rounded-sm">Suggestions</div>
+      {/* <div className=" bg-darkpaco w-full md:flex justify-center items-center px-36 py-16  hidden rounded-sm">{data.date}, </div>
+      <div className=" bg-darkpaco w-full md:flex justify-center items-center px-36 py-16  hidden rounded-sm">{data.number}</div> */}
     </div>
   );
 }
