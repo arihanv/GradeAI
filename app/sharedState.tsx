c9c36289b677b8888fbf147c37ac2e2ae1127b17
TheRedstoneRadiant <76220359+TheRedstoneRadiant@users.noreply.github.com>
2023-07-01 18:55:00 -0400

    Add files via upload

---
+++ app/sharedState.tsx
@@ -0,0 +1,13 @@
+import { atom } from "jotai";
+//set processing for pdf and chat
+export const rubricAtom = atom("");
+
+export const feedbackAtom = atom("");
+
+export const instructionsAtom = atom("");
+
+export const courseAtom = atom("");
+
+export const scoreAtom = atom(0);
+
+export const essayAtom = atom("");
