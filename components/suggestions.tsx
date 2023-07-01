93fd3a2df8d689156be4836b4428a0d6b56d5a1d
arihanv <arihanvaranasi@gmail.com>
2023-07-01 17:32:31 -0500

    Delete Suggestions

--- components/suggestions.tsx
+++ components/suggestions.tsx
@@ -21,8 +21,6 @@ export default function Suggestions({}: Props) {
 
   return (
     <div className="w-full flex flex-col gap-5">
-      <div className=" bg-darkpaco w-full md:flex justify-center items-center px-36 py-16  hidden rounded-sm">Edit Rubric</div>
-      <div className=" bg-darkpaco w-full md:flex justify-center items-center px-36 py-16  hidden rounded-sm">Something</div>
     </div>
   );
 }
